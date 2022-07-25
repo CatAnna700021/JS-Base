db993e24383e79a115558ff149bd3c56d49b1eae
Anna <101825353+CatAnna700021@users.noreply.github.com>
2022-07-25 20:36:29 +0300

    Delete 4.Objects and Functions directory

--- 4.Objects and Functions/Task№5.js
+++
@@ -1,8 +0,0 @@
- function simple (num) {
-     if (num % 1 == 0 && num % num == 0){
-        console.log ("число простое") 
-    }else{
-        console.log("число не простое")
-    }
-}
-let peremen = simple(13.66)
