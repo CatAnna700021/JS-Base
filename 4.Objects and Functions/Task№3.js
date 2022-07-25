9a0b1d52d99eb8072a73df6fd70839543b32aa9d
Anna <101825353+CatAnna700021@users.noreply.github.com>
2022-07-25 20:14:10 +0300

    Delete 4.Objects and Functions directory

--- 4.Objects and Functions/Task№3.js
+++
@@ -1,9 +0,0 @@
-function terminal (yourName){
-if (yourName === "Ann"){
-   console.log("привет" + yourName)
-}else {
-   console.log("привет!")
-}
-}
-let result = terminal  ("Ann")
-console.log(terminal)
--- 4.Objects and Functions/Task№4.js
+++
@@ -1,5 +0,0 @@
-function type (parametr){
-   console.log("тип аргумента:" + typeof parametr )
-}
- let result = type(23);
- 
