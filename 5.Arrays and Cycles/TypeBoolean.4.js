77fc786f25edac965d8b97baf14d937eda632fa6
Anna <101825353+CatAnna700021@users.noreply.github.com>
2022-07-25 20:22:15 +0300

    Delete 5.Arrays and Cycles directory

--- 5.Arrays and Cycles/TypeBoolean.4.js
+++
@@ -1,6 +0,0 @@
-let numbers = [0,1,2,3,0,5,6,0,7,8,9,0];
-var numberToBoolean = [];
-for(let i = 0; i < numbers.length; i++){
- numberToBoolean[i] = Boolean(numbers[i]);
-};
-console.log(numberToBoolean);
