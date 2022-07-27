5d6d3ef956bb72406774a3f264516293d3879cfa
Anna <101825353+CatAnna700021@users.noreply.github.com>
2022-07-27 12:52:48 +0300

    Update and rename Index.7.js to Index.js

--- 5.Arrays and Cycles/Index.js
+++ 5.Arrays and Cycles/Index.js
@@ -1,10 +1,8 @@
 function task(arr, number){
-  for (let i=0; i < arr.length; i++){
-    if(arr[i] == number){
-      console.log(i);
-    }
-  }
-}
+for (let i=0; i < arr.length; i++){
+if(arr[i] == number){
+console.log(i);}}}
+
 arrNumb1 = [10,20,30,40,50,60,70];
 task(arrNumb1, 20)
 task(arrNumb1, 60)
