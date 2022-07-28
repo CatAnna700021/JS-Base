dc8f531fd6b44fe868c0bc0cb80a32ffbdf92d55
Anna <101825353+CatAnna700021@users.noreply.github.com>
2022-07-28 10:35:33 +0300

    Update and rename 4.Objects and Functions/TheNumberIsSimple.js to 4.Объекты и функции/TheNumberIsSimple.js

---
+++ 4.Объекты и функции/TheNumberIsSimple.js
@@ -0,0 +1,6 @@
+ function simple (num) {
+ if (num % 1 == 0 && num % num == 0){
+ console.log ("the number is simple") 
+ }else{
+ console.log("the number is not simple")}}
+let peremen = simple(13.66)
